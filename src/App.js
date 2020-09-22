24386b919c617c0594b39cbf507eebdeb3c4aeef
sigmund <mayitoukousigmund@gmail.com>
2020-09-22 03:02:36 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          By sigmund!
+          I was changed on the the feature branch
         </p>
       </div>
     );
